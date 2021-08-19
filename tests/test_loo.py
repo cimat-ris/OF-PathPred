bb09b9451abe0b1bb2664b61ba076c35dc371cdc
Jean Bernard Hayet <jbhayet@cimat.mx>
2021-08-18 21:19:27 -0500

    Simplified the test file

--- tests/test_loo.py
+++ tests/test_loo.py
@@ -7,16 +7,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras import models
-from path_prediction.datasets_utils import setup_loo_experiment
-from path_prediction.models.model_multimodal_attention import TrajectoryEncoderDecoder, ModelParameters
-from path_prediction.plot_utils import plot_training_data,plot_training_results
-import path_prediction.batches_data
-from path_prediction.testing_utils import evaluation_minadefde,evaluation_qualitative,evaluation_attention,plot_comparisons_minadefde, get_testing_batch
-from path_prediction.training_utils import training_loop
-from path_prediction.training_utils import Experiment_Parameters
-
+from tensorflow.keras import layers, models
+from path_prediction import models, utils
 
 def main():
     # Parsing arguments
@@ -54,26 +46,22 @@ def main():
         logging.info("Using CPU")
 
     # Load the default parameters
-    experiment_parameters = Experiment_Parameters(add_kp=False,obstacles=args.obstacles)
+    experiment_parameters = utils.training_utils.Experiment_Parameters(add_kp=False,obstacles=args.obstacles)
 
     dataset_dir   = args.path
     dataset_names = ['eth-hotel','eth-univ','ucy-zara01','ucy-zara02','ucy-univ']
 
     # Load the dataset and perform the split
     idTest = args.dataset_id
-    training_data,validation_data,test_data,test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,idTest,experiment_parameters,use_pickled_data=args.pickle)
+    training_data,validation_data,test_data,test_homography = utils.datasets_utils.setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,idTest,experiment_parameters,use_pickled_data=args.pickle)
 
-    # Plot ramdomly a subset of the training data (spatial data only)
-    show_training_samples = False
-    if show_training_samples:
-        plot_training_data(training_data,experiment_parameters)
 
     #############################################################
     # Model parameters
-    model_parameters = ModelParameters(add_kp=experiment_parameters.add_kp,add_social=args.social,rnn_type=args.rnn)
+    model_parameters = models.model_multimodal_attention.ModelParameters(add_kp=experiment_parameters.add_kp,add_social=args.social,rnn_type=args.rnn)
     model_parameters.num_epochs     = args.epochs
     # 9 samples generated
-    model_parameters.output_var_dirs= 1
+    model_parameters.output_var_dirs= 5
     model_parameters.is_mc_dropout  = False
     model_parameters.initial_lr     = 0.03
     model_parameters.enc_hidden_size= 128  # Hidden size of the RNN encoder
@@ -97,7 +85,7 @@ def main():
     batched_test_data  = test_data.batch(model_parameters.batch_size)
 
     # Model
-    tj_enc_dec = TrajectoryEncoderDecoder(model_parameters)
+    tj_enc_dec = models.model_multimodal_attention.TrajectoryEncoderDecoder(model_parameters)
 
     # Checkpoints
     checkpoint_dir   = './training_checkpoints/ofmodel'
@@ -110,9 +98,9 @@ def main():
     plot_training    = True
     if args.noretrain==False:
         logging.info("Training the model")
-        train_loss_results,val_loss_results,val_metrics_results,__ = training_loop(tj_enc_dec,batched_train_data,batched_val_data,model_parameters,checkpoint,checkpoint_prefix)
+        train_loss_results,val_loss_results,val_metrics_results,__ = utils.training_utils.training_loop(tj_enc_dec,batched_train_data,batched_val_data,model_parameters,checkpoint,checkpoint_prefix)
         if plot_training==True:
-            plot_training_results(train_loss_results,val_loss_results,val_metrics_results)
+            utils.plot_utils.plot_training_results(train_loss_results,val_loss_results,val_metrics_results)
 
     # Testing
     # Restoring the latest checkpoint in checkpoint_dir
@@ -123,8 +111,8 @@ def main():
     quantitative = True
     if quantitative==True:
         logging.info("Quantitative testing")
-        results = evaluation_minadefde(tj_enc_dec,batched_test_data,model_parameters)
-        plot_comparisons_minadefde(results,dataset_names[idTest])
+        results = utils.testing_utils.evaluation_minadefde(tj_enc_dec,batched_test_data,model_parameters)
+        utils.testing_utils.plot_comparisons_minadefde(results,dataset_names[idTest])
         logging.info(results)
 
     # Qualitative testing
@@ -132,9 +120,8 @@ def main():
     if qualitative==True:
         logging.info("Qualitative testing")
         for i in range(5):
-            batch, test_bckgd = get_testing_batch(test_data,dataset_dir+dataset_names[idTest])
-            #evaluation_qualitative(tj_enc_dec,batch,model_parameters,background=test_bckgd,homography=test_homography, flip=False,n_peds_max=1,display_mode=None)
-            evaluation_attention(tj_enc_dec,batch,model_parameters,background=test_bckgd,homography=test_homography, flip=False,display_mode=None)
+            batch, test_bckgd = utils.testing_utils.get_testing_batch(test_data,dataset_dir+dataset_names[idTest])
+            utils.testing_utils.evaluation_qualitative(tj_enc_dec,batch,model_parameters,background=test_bckgd,homography=test_homography, flip=False,n_peds_max=1,display_mode=None)
 
 
 
