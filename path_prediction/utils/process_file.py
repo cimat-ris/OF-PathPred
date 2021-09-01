Corrected a bug in the rotation of trajectories

path_prediction/utils/process_file.py
@@ -457,25 +457,26 @@ def prepare_data(datasets_path, datasets_names, parameters):
     # Upper level (all datasets)
     # Concatenate all the content of the lists (pos/relative pos/frame ranges)
     seq_pos_all           = np.concatenate(seq_pos_all, axis=0)
+    # By broadcasting, center these data
     seq_pos_centered_all  = seq_pos_all - seq_pos_all[:,obs_len-1:obs_len,0:2]
-    # Note that padding is done at the first displacement with the second displacement
+    # Displacements
+    seq_rel_all           = np.zeros_like(seq_pos_all)
+    seq_rel_all[:,1:,:]   = seq_pos_all[:,1:,:]-seq_pos_all[:,:-1,:]
+    seq_rel_all[:,0,:]    = seq_rel_all[:,1,:]
     # All directions
     seq_theta_all         = np.zeros_like(seq_pos_all[:,:,0:1])
-    seq_theta_all[:,1:,0] = np.arctan2(seq_pos_all[:, 1:, 1] - seq_pos_all[:,:-1, 1],seq_pos_all[:,1:, 0] - seq_pos_all[:,:-1, 0])
-    seq_theta_all[:,0,  0]= seq_theta_all[:,1,  0]
+    seq_theta_all[:,:,0]  = np.arctan2(seq_rel_all[:,:,1],seq_rel_all[:,:,0])
     # Cosine and sine of the orientation angle at the last observed point
-    costheta              = np.cos(seq_theta_all[:,obs_len-1:obs_len,0:1]-math.pi*0.5)
-    sintheta              = np.sin(seq_theta_all[:,obs_len-1:obs_len,0:1]-math.pi*0.5)
+    costheta              = np.cos(seq_theta_all[:,obs_len-1:obs_len,0:1])
+    sintheta              = np.sin(seq_theta_all[:,obs_len-1:obs_len,0:1])
     seq_pos_rot_all       = np.zeros_like(seq_pos_all)
     seq_pos_rot_all[:,:,0:1]= costheta*(seq_pos_centered_all[:,:,0:1])+sintheta*(seq_pos_centered_all[:,:,1:2])
     seq_pos_rot_all[:,:,1:2]=-sintheta*(seq_pos_centered_all[:,:,0:1])+costheta*(seq_pos_centered_all[:,:,1:2])
     # All the displacements are estimated here.
     seq_rel_rot_all         = np.zeros_like(seq_pos_rot_all)
     seq_rel_rot_all[:,1:,:] = seq_pos_rot_all[:,1:,:]-seq_pos_rot_all[:,:-1,:]
+    # Note that padding is done at the first displacement with the second displacement
     seq_rel_rot_all[:,0,:]  = seq_rel_rot_all[:,1,:]
-    seq_rel_all             = np.zeros_like(seq_pos_all)
-    seq_rel_all[:,1:,:]     = seq_pos_all[:,1:,:]-seq_pos_all[:,:-1,:]
-    seq_rel_all[:,0,:]      = seq_rel_all[:,1,:]
     seq_frames_all    = np.concatenate(seq_frames_all, axis=0)
     seq_neighbors_all = np.concatenate(seq_neighbors_all, axis=0)
     logging.info("Total number of sample sequences: ".format(len(seq_pos_all)))