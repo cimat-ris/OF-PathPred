0e59e219a88305dedcff6affe26618d039bf0f2c
JB Hayet <jbhayet@cimat.mx>
2021-09-05 23:04:47 -0500

    Take the partition from trajnetplusplus

--- path_prediction/utils/datasets_utils.py
+++ path_prediction/utils/datasets_utils.py
@@ -108,47 +108,15 @@ def setup_trajnetplusplus_experiment(experiment_name,ds_path,train_ds_names,test
     logging.info('Training datasets: {}'.format(train_ds_names))
     if use_pickled_data==False:
         # Process data specified by the path to get the trajectories with
-        logging.info('Extracting data from the datasets')
+        logging.info('Extracting data from the datasets '+ds_path)
         # Note that for the training data, we do not keep the neighbots information (too heavy!)
         train_data, train_primary_path = prepare_data_trajnetplusplus(ds_path+"/train/",train_ds_names,experiment_parameters,keep_neighbors=False)
-        test_data, test_primary_path = prepare_data_trajnetplusplus(ds_path+"/val/",test_ds_names, experiment_parameters)
-
-        # Count how many data we have (sub-sequences of length 8, in pred_traj)
-        n_test_data  = len(test_data[list(test_data.keys())[2]])
-        n_train_data = len(train_data[list(train_data.keys())[2]])
-        idx          = np.random.permutation(n_train_data)
-        # TODO: validation should be done from a similar distribution as test set!
-        validation_pc= validation_proportion
-        validation   = int(n_train_data*validation_pc)
-        training     = int(n_train_data-validation)
-
-        # Indices for training
-        idx_train = idx[0:training]
-        #  Indices for validation
-        idx_val   = idx[training:]
-        # Training set
-        training_data = {
-            "obs_traj":      train_data["obs_traj"][idx_train],
-            "obs_traj_rel":  train_data["obs_traj_rel"][idx_train],
-            "obs_traj_theta":train_data["obs_traj_theta"][idx_train],
-            "pred_traj":     train_data["pred_traj"][idx_train],
-            "pred_traj_rel": train_data["pred_traj_rel"][idx_train]
-        }
-        training_data["obs_optical_flow"]=train_data["obs_optical_flow"][idx_train]
-        # Validation set
-        validation_data ={
-            "obs_traj":      train_data["obs_traj"][idx_val],
-            "obs_traj_rel":  train_data["obs_traj_rel"][idx_val],
-            "obs_traj_theta":train_data["obs_traj_theta"][idx_val],
-            "pred_traj":     train_data["pred_traj"][idx_val],
-            "pred_traj_rel": train_data["pred_traj_rel"][idx_val]
-        }
-        validation_data["obs_optical_flow"]=train_data["obs_optical_flow"][idx_val]
-        validation_data["index"]=train_data["index"][idx_val]
+        test_data, test_primary_path   = prepare_data_trajnetplusplus(ds_path+"/test_private/",test_ds_names, experiment_parameters,keep_neighbors=False)
+        val_data, __                   = prepare_data_trajnetplusplus(ds_path+"/val/",test_ds_names, experiment_parameters,keep_neighbors=False)
 
         # Training dataset
         pickle_out = open(pickle_dir+'/training_data_'+experiment_name+'.pickle',"wb")
-        pickle.dump(training_data, pickle_out, protocol=2)
+        pickle.dump(train_data, pickle_out, protocol=2)
         pickle_out.close()
 
         # Test dataset
@@ -158,19 +126,19 @@ def setup_trajnetplusplus_experiment(experiment_name,ds_path,train_ds_names,test
 
         # Validation dataset
         pickle_out = open(pickle_dir+'/validation_data_'+experiment_name+'.pickle',"wb")
-        pickle.dump(validation_data, pickle_out, protocol=2)
+        pickle.dump(val_data, pickle_out, protocol=2)
         pickle_out.close()
     else:
         # Unpickle the ready-to-use datasets
         logging.info("Unpickling...")
         pickle_in = open(pickle_dir+'/training_data_'+experiment_name+'.pickle',"rb")
-        training_data = pickle.load(pickle_in)
+        train_data = pickle.load(pickle_in)
         pickle_in = open(pickle_dir+'/test_data_'+experiment_name+'.pickle',"rb")
         test_data = pickle.load(pickle_in)
         pickle_in = open(pickle_dir+'/validation_data_'+experiment_name+'.pickle',"rb")
-        validation_data = pickle.load(pickle_in)
+        val_data = pickle.load(pickle_in)
 
-    logging.info("Training data: "+ str(len(training_data[list(training_data.keys())[0]])))
+    logging.info("Training data: "+ str(len(train_data[list(train_data.keys())[0]])))
     logging.info("Test data: "+ str(len(test_data[list(test_data.keys())[0]])))
-    logging.info("Validation data: "+ str(len(validation_data[list(validation_data.keys())[0]])))
-    return training_data,validation_data,test_data, train_primary_path, test_primary_path
+    logging.info("Validation data: "+ str(len(val_data[list(val_data.keys())[0]])))
+    return train_data,val_data,test_data, train_primary_path, test_primary_path
