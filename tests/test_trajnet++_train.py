5ae7e17af54f25c2ab5ad7c69e373e7bf1d918c9
Jean Bernard Hayet <jbhayet@cimat.mx>
2021-09-07 20:14:58 -0500

    Performing tests for trajnetplusplus

--- tests/test_trajnet++_train.py
+++ tests/test_trajnet++_train.py
@@ -32,6 +32,8 @@ def main():
                     type=int, default=35,help='Number of epochs (default: 35)')
     parser.add_argument('--rnn', default='lstm', choices=['gru', 'lstm'],
                     help='recurrent networks to be used (default: "lstm")')
+    parser.add_argument('--coords_mode', default='rel', choices=['rel', 'rel_rot'],
+                    help='coordinates to be used as input (default: "rel")')
 
     # arg for evaluation trajnetplusplus
     parser.add_argument('--output', nargs='+',
@@ -66,30 +68,30 @@ def main():
     else:
         logging.info('Using CPU')
 
-
-    #train_dataset_names = ["biwi_hotel","crowds_students001","crowds_students003","crowds_zara01","crowds_zara03","lcas","wildtrack","cff_06","cff_07","cff_08"]
-    #train_dataset_names = ["biwi_hotel","crowds_students001","crowds_students003"]
-    train_dataset_names = ["biwi_hotel"]
-    test_dataset_names  = ["biwi_hotel"]
-
+    ## List of .json file inside the args.path (waiting to be predicted by the testing model)
+    datasets = sorted([f.split('.')[-2] for f in os.listdir(args.path+'/train') if not f.startswith('.') and f.endswith('.ndjson')])
+    logging.info("Found the following datasets: {}".format(datasets))
+    train_dataset_names = datasets
+    test_dataset_names  = datasets
     # Load the default parameters
     experiment_parameters = utils.training_utils.Experiment_Parameters(obstacles=False)
     experiment_parameters.obs_len  = args.obs_length
     experiment_parameters.pred_len = args.pred_length
     # Load the datasets
-    training_data,validation_data,testing_data, train_primary_path, test_primary_path = utils.datasets_utils.setup_trajnetplusplus_experiment('TRAJNETPLUSPLUS',args.path,train_dataset_names,test_dataset_names,experiment_parameters,use_pickled_data=args.pickle)
+    training_data,validation_data,testing_data, train_primary_path,val_primary_path, test_primary_path = utils.datasets_utils.setup_trajnetplusplus_experiment('TRAJNETPLUSPLUS',args.path,train_dataset_names,test_dataset_names,experiment_parameters,use_pickled_data=args.pickle)
     logging.info("Total number of training trajectories: {}".format(training_data["obs_traj"].shape[0]))
 
     #############################################################
     # Model parameters
-    model_parameters = models.model_multimodal_attention.PredictorMultAtt.Parameters(add_social=args.social,rnn_type=args.rnn)
+    model_parameters = models.model_multimodal_of.PredictorMultOf.Parameters(rnn_type=args.rnn)
     model_parameters.num_epochs     = args.epochs
     # 9 samples generated
-    model_parameters.output_var_dirs= 1
+    model_parameters.add_social     = args.social
+    model_parameters.coords_mode    = args.coords_mode
+    model_parameters.output_var_dirs= 3
     model_parameters.initial_lr     = 0.03
-    model_parameters.enc_hidden_size= 128  # Hidden size of the RNN encoder
-    model_parameters.dec_hidden_size= model_parameters.enc_hidden_size # Hidden size of the RNN decoder
-    model_parameters.emb_size       = 256  # Embedding size
+    model_parameters.pred_length    = 12
+    model_parameters.obs_length     = 9
     model_parameters.use_validation = True
     # When running on CPU
     if len(physical_devices)==0:
@@ -108,7 +110,7 @@ def main():
     batched_test_data  = test_data.batch(model_parameters.batch_size)
 
     # Model
-    tj_enc_dec = models.model_multimodal_attention.PredictorMultAtt(model_parameters)
+    tj_enc_dec = models.model_multimodal_of.PredictorMultOf(model_parameters)
 
     # Checkpoints
     checkpoint_dir   = './training_checkpoints/ofmodel-trajnet++'
@@ -120,20 +122,20 @@ def main():
     # Training
     if args.noretrain==False:
         logging.info("Training the model")
-        train_loss_results,val_loss_results,val_metrics_results,__ = utils.training_utils.training_trajnetplusplus_loop(tj_enc_dec,batched_train_data,batched_val_data,train_primary_path,model_parameters,checkpoint,checkpoint_prefix)
+        train_loss_results,val_loss_results,val_metrics_results,__ = utils.training_utils.training_trajnetplusplus_loop(tj_enc_dec,batched_train_data,batched_val_data,model_parameters,checkpoint,checkpoint_prefix)
         utils.plot_utils.plot_training_results(train_loss_results,val_loss_results,val_metrics_results)
 
     # Testing
     # Restoring the latest checkpoint in checkpoint_dir
     logging.info("Restoring last model")
-    status = checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+    status = checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir)).expect_partial()
 
     # Quantitative testing: ADE/FDE
     quantitative = True
     add_info_other_models = True
 
     if quantitative==True:
-        print("[INF] Quantitative testing")
+        logging.info("Quantitative testing")
 
         # Initiate Result Table
         table = Table()
@@ -142,9 +144,9 @@ def main():
         utils.testing_utils.evaluation_trajnetplusplus_minadefde(tj_enc_dec, batched_test_data, test_primary_path, model_parameters, table=table)
 
         # For add result of model orca, kf, cv, sf
-        if add_info_other_models:
-            args.obs_length = 9
-            utils.testing_utils.other_models(args,table)
+        # if add_info_other_models:
+        #    args.obs_length = 9
+        #    utils.testing_utils.evaluation_trajnetplusplus_other_models(args,table)
 
         ## Save table in a image
         table.print_table()
